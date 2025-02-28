ee3243cd6ccc60fa6fb6d6f7457dcd1f001fed3c
Cuedadar <cuedadar2015@gmail.com>
2025-02-28 15:24:35 -0600

    Finished Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Knowledge Check - Advanced JS / Async / Fetch
+
+This project demonstrates my understanding of fetching from an API and updating the DOM.
+
+**A live version of the site can be found at *[Netlify]()***
+
+The website uses Bootstrap 5 and vanilla Javascript to search the pokeapi https://pokeapi.co/ and display basic results.
+
+The index.html is your starting point into the website. JS and CSS can be found in their respective folders.
+
+If I were to re-visit this project, there's a TON of data attached to each pokemon. It might be fun to build out a styled Pokedex.
---
+++ js/main.js
@@ -0,0 +1,63 @@
+//
+// Place any custom JS here
+//
+async function searchPokemon(searchTerm) {
+    try {
+        let result = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
+        let data = await result.json();
+        updateResults(data);
+    } catch (error) {
+        showError(error)
+    }
+}
+
+function updateResults(data) {
+    let mainElement = document.getElementById('main');
+
+    //Handle multiple types
+    let typeString = "";
+    data.types.map(type => typeString += `${type.type.name} `);
+    typeString = typeString.trim();
+
+    //Handle appears in
+    let appearsInString = "";
+    data.game_indices.map(game => appearsInString += `<li>${game.version.name}</li>`);
+
+    mainElement.innerHTML = `
+        <div class="d-flex">
+            <img src="${data.sprites.front_default}" alt="Pokemon Sprite">
+            <div class="d-flex flex-column justify-content-center align-items-center">
+                <h2 class="capitalized">${data.name}</h2>
+                <p class="capitalized my-1">${typeString}</p>
+            </div>
+        </div>
+        <div class="d-flex mt-2">
+            <div class="col d-flex flex-column">
+                <h2>Appears In</h2>
+                <ul class="capitalized list-unstyled centered-text">${appearsInString}</ul>
+            </div>
+        </div>
+        
+    `;
+}
+
+function showError(error) {
+    console.log(error);
+
+    let mainElement = document.getElementById('main');
+    mainElement.innerHTML = `
+        <div class="col d-flex mx-auto">
+            <h2 class="mx-0">Pokemon Not Found or Server Error</h2>
+        </div>
+        
+    `;
+}
+
+// Handle search term on form submission (could theoretically use button click event
+//  if search was not a form
+const form = document.getElementById('searchForm');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let formData = new FormData(form);
+    searchPokemon(formData.get('searchTerm'));
+});
